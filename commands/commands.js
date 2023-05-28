89854b9258747b5259aeee1c676f910fd4805962
mtdev94 <83127061+mtdev94@users.noreply.github.com>
2023-05-28 21:17:25 +0200

    First commit

---
+++ commands/commands.js
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import { InstallGlobalCommands } from "../utils.js";
+
+
+const LP_EMPLOYEE = {
+    name: "invoke-employee",
+    description: "Invoke the LP-Bot in an employee channel",
+    type: 1,
+}
+
+const LP_MANAGEMENT = {
+    name: "invoke-management",
+    description: "Invoke the LP-Bot in a management channel",
+    type: 1,
+}
+
+const ALL_COMMANDS = [LP_EMPLOYEE, LP_MANAGEMENT];
+
+InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
---
+++ index.js
@@ -0,0 +1,292 @@
+import {
+  Client,
+  Events,
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonStyle,
+  ButtonBuilder,
+} from "discord.js";
+import { LpRunner } from "./lp-runner.js";
+
+const client = new Client({
+  intents: [],
+});
+
+const token =
+  "MTExMTk4NzM1MDI4MTY1MDE4Nw.GAGLkB.emjo9Ud2amlnuFnGVla8vNMWtnleqdilCn8mdQ";
+
+const lpRunner = new LpRunner(runnerCallback);
+
+var managerMessage;
+var employeeMessage;
+var embedManagementInteraction;
+var embedEmployeeInteraction;
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (interaction.isChatInputCommand()) {
+    if (interaction.commandName === "invoke-management") {
+      const embed = prepareEmbedManagement(interaction);
+
+      interaction
+        .reply({
+          embeds: [embed],
+          components: [prepareButtonsManagement()],
+        })
+        .then((result) => {
+          managerMessage = result;
+        })
+        .catch((err) => {});
+    }
+
+    if (interaction.commandName === "invoke-employee") {
+      const embed = prepareEmbedEmployees(interaction);
+
+      await interaction
+        .reply({
+          embeds: [embed],
+          components: [prepareButtonsEmployee()],
+        })
+        .then((result) => {
+          employeeMessage = result;
+        })
+        .catch((err) => {});
+    }
+  }
+
+  if (interaction.isButton) {
+    if (interaction.customId === "clock-in") {
+      lpRunner.clockEmployeeIn({
+        id: interaction.user.id,
+        username: interaction.user.username,
+      });
+
+      const exampleEmbed = prepareEmbedEmployees(interaction);
+      await interaction.update({
+        embeds: [exampleEmbed],
+        components: [prepareButtonsEmployee()],
+      });
+    }
+
+    if (interaction.customId === "clock-out") {
+      lpRunner.clockEmployeeOut({
+        id: interaction.user.id,
+        username: interaction.user.username,
+      });
+
+      const exampleEmbed = prepareEmbedEmployees(interaction);
+      await interaction.update({
+        embeds: [exampleEmbed],
+        components: [prepareButtonsEmployee()],
+      });
+    }
+
+    if (interaction.customId === "open-lp") {
+      console.log(`coming from \n${interaction.message}`);
+      lpRunner.open_lp();
+
+      const exampleEmbed = prepareEmbedManagement(interaction);
+      await interaction.update({
+        embeds: [exampleEmbed],
+        components: [prepareButtonsManagement()],
+      });
+    }
+
+    if (interaction.customId === "close-lp") {
+      lpRunner.close_lp();
+
+      const embed = prepareEmbedManagement(interaction);
+      const report = prepareReport(interaction);
+
+      await managerMessage.delete();
+
+      await interaction.reply({
+        embeds: [report],
+      });
+    }
+  }
+});
+
+function prepareButtonsEmployee() {
+  const confirm = new ButtonBuilder()
+    .setCustomId("clock-in")
+    .setLabel("Clock In")
+    .setStyle(ButtonStyle.Success)
+    .setDisabled(!lpRunner.is_open);
+
+  const cancel = new ButtonBuilder()
+    .setCustomId("clock-out")
+    .setLabel("Clock Out")
+    .setStyle(ButtonStyle.Danger)
+    .setDisabled(!lpRunner.is_open);
+
+  const row = new ActionRowBuilder().addComponents(confirm, cancel);
+
+  return row;
+}
+
+function prepareButtonsManagement() {
+  const confirm = new ButtonBuilder()
+    .setCustomId("open-lp")
+    .setLabel("Open LP")
+    .setStyle(ButtonStyle.Success)
+    .setDisabled(lpRunner.is_open);
+
+  const cancel = new ButtonBuilder()
+    .setCustomId("close-lp")
+    .setLabel("Close LP")
+    .setStyle(ButtonStyle.Danger)
+    .setDisabled(!lpRunner.is_open);
+
+  const row = new ActionRowBuilder().addComponents(confirm, cancel);
+
+  return row;
+}
+
+function prepareEmbedManagement(interaction) {
+  const builder = new EmbedBuilder();
+  const date = new Date().toDateString();
+
+  embedManagementInteraction = interaction;
+
+  builder
+    .setColor("Orange")
+    .setTitle(`LP is ${lpRunner.is_open ? "Open!" : "Closed!"}`)
+    .setDescription(
+      `Employees clocked in (${lpRunner.employees_clockedIn.size}/3) - ${date}
+      Tickets generated ${lpRunner.ticketsGenerated}`
+    );
+
+  lpRunner.employees.forEach((e) => {
+    builder.addFields({
+      name: `${e.username} - ${e.tickets} ticket(s)`,
+      value: `${
+        e.clocked_in
+          ? `${e.date.toLocaleString("en-US", {
+              timeZone: "America/New_York",
+              hour: "numeric",
+              minute: "numeric",
+              hour12: true,
+            })} EST`
+          : "Not clocked-in"
+      }`,
+      inline: true,
+    });
+  });
+
+  builder.setTimestamp().setFooter({
+    text: `${interaction.user.username} Opened the lucky plucker`,
+    iconURL: `https://cdn.discordapp.com/avatars/${interaction.user.id}/${interaction.user.avatar}.png?size=256`,
+  });
+
+  return builder;
+}
+
+function prepareEmbedEmployees(interaction) {
+  const builder = new EmbedBuilder();
+  const date = new Date().toDateString();
+
+  embedEmployeeInteraction = interaction;
+
+  builder
+    .setColor("Orange")
+    .setTitle(`LP is ${lpRunner.is_open ? "Open" : "Closed"}!`)
+    .setDescription(
+      `Employees clocked in - ${lpRunner.employees_clockedIn.size}/3`
+    );
+
+  lpRunner.employees_clockedIn.forEach((e) => {
+    builder.addFields({
+      name: `${e.username} - ${e.tickets} ticket(s)`,
+      value: `${e.date.toLocaleString("en-US", {
+        timeZone: "America/New_York",
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true,
+      })} EST`,
+      inline: false,
+    });
+  });
+
+  builder.setTimestamp();
+
+  return builder;
+}
+
+function prepareReport(interaction) {
+  const builder = new EmbedBuilder();
+  const date = new Date().toDateString();
+
+  embedManagementInteraction = interaction;
+
+  builder
+    .setColor("Orange")
+    .setTitle(`LP is now closed!`)
+    .setDescription(`Here's the report`);
+
+  builder.addFields({
+    name: "Total tickets generated",
+    value: `${lpRunner.ticketsGenerated}`,
+  });
+
+  lpRunner.employees.forEach((e) => {
+    builder.addFields({
+      name: e.username,
+      value: `${e.time_elapsed} minute(s) - ${e.tickets} ticket(s)`,
+      inline: false,
+    });
+  });
+
+  builder.setTimestamp().setFooter({
+    text: `${interaction.user.username} Opened the lucky plucker`,
+    iconURL: `https://cdn.discordapp.com/avatars/${interaction.user.id}/${interaction.user.avatar}.png?size=256`,
+  });
+
+  return builder;
+}
+
+async function runnerCallback(name) {
+  if (name === "open_lp" || name === "close_lp") {
+    if (!employeeMessage) {
+      return;
+    }
+    const embed = prepareEmbedEmployees();
+
+    await employeeMessage.edit({
+      embeds: [embed],
+      components: [prepareButtonsEmployee()],
+    });
+  }
+
+  if (
+    name === "clockEmployeeIn" ||
+    name === "clockEmployeeOut" ||
+    name === "handleTimer"
+  ) {
+    if (!managerMessage) {
+      return;
+    }
+    const embed = prepareEmbedManagement(embedManagementInteraction);
+
+    await managerMessage.edit({
+      embeds: [embed],
+      components: [prepareButtonsManagement()],
+    });
+
+    if (!employeeMessage) {
+      return;
+    }
+
+    const embedEmployee = prepareEmbedEmployees();
+
+    await employeeMessage.edit({
+      embeds: [embedEmployee],
+      components: [prepareButtonsEmployee()],
+    });
+  }
+}
+
+client.login(token);
